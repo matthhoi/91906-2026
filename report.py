ac17600f252233cd9fd3122a405d3184499a04ef
matthhoi <smithm4@stu.otc.school.nz>
2026-05-11 09:46:37 +1200

    folowed pip8

--- report.py
+++ report.py
@@ -1,4 +1,4 @@
-"""This program is genarating a report based on the info given by the calling 
+"""This program is genarating a report based on the info given by the calling
 program
 By: Matt Smith                                                    06/05/2026"""
 
@@ -9,14 +9,17 @@ from datetime import date
 # global variables
 today = date.today()
 
-# Main program 
-def report(inventory_list):
+# Main program
+
+
+def report(inventory_list, staff_name):
     # generating reports
     with open(f"report-{today.day}.{today.month}.{today.year}.txt", "w") as report:
         # Header Section
         report.write(f"{'-------------------- Report --------------------':^85}"
-                    f"\n{'Barcode':<15} {'Name':<20} {'(Qty)':<10} {'Old':<10}"
-                    f" {'Purch':<10} {'Total':<10} {'Sold':<10} {'New':<8}\n")
+                     f"\n{'Barcode':<15} {'Name':<20} {'(Qty)':<10} "
+                     f"{'Old':<10} {'Purch':<10} {'Total':<10} {'Sold':<10} "
+                     f"{'New':<8}\n")
         report.write("-" * 100 + "\n")
 
         total_old = 0
@@ -36,7 +39,7 @@ def report(inventory_list):
             inv_value += item[4]*item[5]
             total_sold += item[2]+item[3]-item[4]
             total_sales += (item[2]+item[3])*item[5]
-            item_name = item[1][:25] # Truncate name if too long
+            item_name = item[1][:25]  # Truncate name if too long
             report.write(f"{item[0]:<15} {item_name:<20} {'':<10} {item[2]:<10}"
                          f" {item[3]:<10} {(item[2]+item[3]):<10} "
                          f"{(item[2]+item[3]-item[4]):<10} {item[4]:<8}\n")
@@ -49,10 +52,11 @@ def report(inventory_list):
         report.write("-" * 100 + "\n")
         report.write(f"{total_old:<15} {total_new:<15} ${inv_value:<19.2f} "
                      f"{total_sold:<15} ${total_sales:<14.2f}\n")
-        
+
         # Date Footer
         report.write("-" * 100 + "\n")
         report.write(f"Date: {today.day}/{today.month}/{today.year}\n")
+        report.write(f"User: {staff_name}\n")
         report.write("-" * 100)
 
-        messagebox.showinfo("Success", "Report saved successfully!") 
+        messagebox.showinfo("Success", "Report saved successfully!")
